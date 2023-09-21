c9d453cbe3341a2ff57032d84738daf495ad758b
ozckarr <lindbladoskar@gmail.com>
2023-09-21 08:48:42 +0200

    single boxes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./style/style.css";
 import Menu from "./components/Menu";
+//import Example from "./components/Example";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="App">
       <Menu />
-      lok
+      <Board />
     </div>
   );
 }
--- src/components/Menu.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const menu = () => {
-  return <div>korv</div>;
-};
-
-export default menu;
